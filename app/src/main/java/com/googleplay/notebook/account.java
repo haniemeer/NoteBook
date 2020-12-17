7054c5ed6f0c87e7cd69ddaf204dc1e6f0aaa98e
hanieh <haniemeer80@gmail.com>
2020-12-17 03:17:06 -0800

    Completed onclick listener

--- app/src/main/java/com/googleplay/notebook/account.java
+++ app/src/main/java/com/googleplay/notebook/account.java
@@ -3,9 +3,11 @@ package com.googleplay.notebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -28,7 +30,11 @@ public class account extends AppCompatActivity {
             public void onClick(View v) {
                 String emailtext=emailaccount.getText().toString();
                 String passwordtext=passaccount.getText().toString();
-
+                     if (TextUtils.isEmpty(emailtext)||TextUtils.isEmpty(passwordtext)){
+                         Toast.makeText(account.this,"fields are not filled",Toast.LENGTH_LONG).show();
+                     }
+                    // else
+                         //accountlogin(emailtext,passwordtext);
             }
         });
     }
