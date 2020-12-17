1ea7b2286e6b8704294fffb9f283834eacdd75d7
hanieh <haniemeer80@gmail.com>
2020-12-17 03:07:27 -0800

    got ids for account objects. set clicklistener for buttonlogin

--- app/src/main/java/com/googleplay/notebook/account.java
+++ app/src/main/java/com/googleplay/notebook/account.java
@@ -3,12 +3,34 @@ package com.googleplay.notebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class account extends AppCompatActivity {
+    EditText emailaccount;
+    EditText passaccount;
+    Button buttonaccount;
+    FirebaseAuth firebaseauth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        emailaccount=findViewById(R.id.editemail);
+        passaccount=findViewById(R.id.editpass);
+        firebaseauth=FirebaseAuth.getInstance();
+        buttonaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailtext=emailaccount.getText().toString();
+                String passwordtext=passaccount.getText().toString();
+
+            }
+        });
     }
+
 }
